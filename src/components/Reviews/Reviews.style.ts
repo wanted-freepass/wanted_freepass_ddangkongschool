fc0cfcbc23fc8a187f5215a4c18203971e0ff66c
홍유진 <hongyoujin@hong-yujin-ui-iMac.local>
2022-02-15 15:01:54 +0900

    Pull: carousel

---
+++ src/components/Reviews/Reviews.style.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  width: 100%;
+  height: 703px;
+  left: 0px;
+  top: 0px;
+  border-radius: 0px;
+  position: relative;
+`;
+
+export const Black = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  position: absolute;
+  left: 0;
+  top: 0;
+  opacity: 0.7;
+`;
+
+export const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url('/images/image 22.png');
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: -1;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next';
 import Nav from '../components/Nav/Nav';
+import Reviews from '../components/Reviews/Reviews';
 
 const Home: NextPage = () => {
   return (
     <>
       <div>안녕</div>
       <Nav />
+      <Reviews />
     </>
   );
 };
