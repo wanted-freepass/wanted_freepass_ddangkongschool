a2ffed9c04908e6f8c70e45b84adaad26b96a95d
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-02-16 01:20:57 +0900

    Add : checked 배너 반응형 웹 완료

--- src/components/CheckedBanner/CheckedBanner.style.tsx
+++ src/components/CheckedBanner/CheckedBanner.style.tsx
@@ -5,12 +5,29 @@ export const Container = styled.section`
   width: 100%;
   height: 647px;
   background-color: ${props => props.theme.lightGray};
+
+  @media ${({ theme }) => theme.laptop} {
+    height: 820px;
+    text-align: center;
+  }
+
+  @media ${({ theme }) => theme.tablet} {
+    height: 655px;
+  }
 `;
 
 export const ContentWrap = styled.div`
   ${props => props.theme.flex('center', 'space-between')};
   max-width: 1140px;
   margin: 0 auto;
+
+  @media ${({ theme }) => theme.laptop} {
+    ${props => props.theme.flex('center', 'center', 'column')};
+    height: 100%;
+  }
+
+  @media ${({ theme }) => theme.tablet} {
+  }
 `;
 
 export const TextWrap = styled.article``;
@@ -20,12 +37,24 @@ export const Title = styled.h1`
   font-weight: ${props => props.theme.weightBold};
   font-size: ${props => props.theme.fontLarge};
   line-height: 70px;
+
+  @media ${({ theme }) => theme.tablet} {
+    font-weight: ${props => props.theme.weightSemiBold};
+    font-size: ${props => props.theme.fontMedium};
+    line-height: 30px;
+  }
 `;
 
 export const Text = styled.p`
   font-weight: ${props => props.theme.weightRegular};
   font-size: ${props => props.theme.fontSemiMedium};
   line-height: 40px;
+
+  @media ${({ theme }) => theme.tablet} {
+    font-weight: ${props => props.theme.weightRegular};
+    font-size: ${props => props.theme.fontSmall};
+    line-height: 20px;
+  }
 `;
 
 export const CheckedContainer = styled.div`
@@ -34,6 +63,16 @@ export const CheckedContainer = styled.div`
   right: -34px;
   width: 788px;
   height: 566px;
+
+  @media ${({ theme }) => theme.laptop} {
+    right: 0;
+    width: 90%;
+  }
+
+  @media ${({ theme }) => theme.tablet} {
+    width: 420px;
+    margin: 0 auto;
+  }
 `;
 
 export const BookContainer = styled.div`
@@ -44,6 +83,10 @@ export const BookContainer = styled.div`
 export const BookWrap = styled.div`
   margin-left: 50px;
   ${props => props.theme.flex('center', 'center', 'column')};
+
+  @media ${({ theme }) => theme.tablet} {
+    margin-left: 20px;
+  }
 `;
 
 export const CheckedWrap = styled.div`
--- src/components/CheckedBanner/CheckedBanner.tsx
+++ src/components/CheckedBanner/CheckedBanner.tsx
@@ -8,13 +8,12 @@ const CheckedBanner = () => {
       <S.ContentWrap>
         <S.TextWrap>
           <S.Title>
-            교육 전문가들이
-            <br />
+            교육 전문가들이 <br />
             선정한 도서
           </S.Title>
           <S.Text>
-            호두랩스의 교육 전문가들이
-            <br /> 교과 수록, 초등 필독 도서 등<br />
+            호두랩스의 교육 전문가들이 <br />
+            교과 수록, 초등 필독 도서 등<br />
             아동 교육에 적합한 도서를
             <br />
             직접 선정해요!
